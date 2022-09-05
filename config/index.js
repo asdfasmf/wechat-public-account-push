971c8b2a309a5ebe6ed1f2890d61500d6bc87781
asdfasmf <112846723+asdfasmf@users.noreply.github.com>
2022-09-05 15:35:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -33,9 +33,9 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "LMD-uEAg_e3wZUc9G8Y0ETLb0czSRXNnN8iwe483ZwQ",
       // 所在省份
-      province: "内蒙古自治区",
+      province: "安徽省",
       // 所在城市
-      city: "巴彦淖尔市",
+      city: "阜阳市",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
@@ -89,9 +89,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "内蒙古自治区",
+  PROVINCE: "安徽省",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "巴彦淖尔市",
+  CITY: "阜阳市",
 
   /** 重要节日相关 */
 
