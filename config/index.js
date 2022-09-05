7cf116eb9f875b2ac5da7b23f1925950216c69d6
asdfasmf <112846723+asdfasmf@users.noreply.github.com>
2022-09-05 15:32:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ export const config = {
       // 所在省份
       province: "内蒙古自治区",
       // 所在城市
-      city: "磴口县",
+      city: "巴彦淖尔市",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
@@ -91,7 +91,7 @@ export const config = {
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
   PROVINCE: "内蒙古自治区",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "磴口县",
+  CITY: "巴彦淖尔市",
 
   /** 重要节日相关 */
 
